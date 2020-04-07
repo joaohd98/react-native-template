b2d5be673e961b72201efe876cd108218b3b51f9
João Damazio <joaohd98@gmail.com>
2020-04-07 10:06:04 -0300

    Add enzyme

--- __tests__/screens/Login.tsx
+++ __tests__/screens/Login.tsx
@@ -1,5 +1,6 @@
 import "react-native";
 import React from "react";
+import {mount} from "enzyme";
 
 // Note: test renderer must be required after react-native.
 import renderer from "react-test-renderer";
@@ -9,5 +10,12 @@ describe("LoginScreen", async () => {
   it("renders correctly", async () => {
     renderer.create(<LoginScreen />);
   });
-
+  /*
+   * BDD Description
+   */
+  it("renders correctly", async () => {
+    const app = mount(<LoginScreen />);
+    const text = app.find("Text").at(0).text();
+    expect(text).toEqual("Aluno");
+  });
 });
--- jest-setup-file.js
+++ jest-setup-file.js
@@ -1,5 +1,10 @@
+import {configure} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
 const mockAsyncStorage = require("@react-native-community/async-storage/jest/async-storage-mock");
 const mockRNCNetInfoMock = require("@react-native-community/netinfo/jest/netinfo-mock");
 
 jest.mock("@react-native-community/netinfo", () => mockRNCNetInfoMock);
 jest.mock("@react-native-community/async-storage", () => mockAsyncStorage);
+
+configure({adapter: new Adapter()});
